e8e26893a7dc6cd87136cf5d87f46d26d716d252
ammodit-p <ammodit@yandex.ru>
2023-10-08 16:36:15 +0300

    fix game prime

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -5,7 +5,7 @@ function isPrime(num) {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i < num; i += i) {
+  for (let i = 2; i < num; i += 1) {
     if ((num % i) === 0) {
       return false;
     }
